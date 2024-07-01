c3f55ede41fef2c6a52c64484306c98a46530d03
acpiccolo <60399193+acpiccolo@users.noreply.github.com>
2024-07-01 08:30:50 +0200

    chore: change error

--- src/error.rs
+++ src/error.rs
@@ -3,8 +3,6 @@ use std::fmt;
 #[derive(Debug)]
 pub enum Error {
     RangeError,
-    Error(tokio_modbus::Error),
-    Exception(tokio_modbus::Exception),
 }
 
 impl std::error::Error for Error {}
@@ -12,23 +10,7 @@ impl std::error::Error for Error {}
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            // Both underlying errors already impl `Display`, so we defer to
-            // their implementations.
-            Error::Error(ref err) => write!(f, "Modbus error: {}", err),
-            Error::Exception(ref err) => write!(f, "Modbus exception: {}", err),
             Error::RangeError => write!(f, "Value out of range"),
         }
-    }
-}
-
-impl From<tokio_modbus::Error> for Error {
-    fn from(err: tokio_modbus::Error) -> Error {
-        Error::Error(err)
-    }
-}
-
-impl From<tokio_modbus::Exception> for Error {
-    fn from(err: tokio_modbus::Exception) -> Error {
-        Error::Exception(err)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,14 @@ mod error;
 pub use error::Error;
 pub mod protocol;
 
+#[cfg(any(
+    feature = "tokio-rtu-sync",
+    feature = "tokio-tcp-sync",
+    feature = "tokio-rtu",
+    feature = "tokio-tcp"
+))]
+pub mod tokio_error;
+
 #[cfg(any(feature = "tokio-rtu-sync", feature = "tokio-tcp-sync"))]
 pub mod tokio_sync_client;
 

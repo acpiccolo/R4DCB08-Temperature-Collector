5fea4b61bd380d7a6910f50a5d1846c3f095663f
acpiccolo <60399193+acpiccolo@users.noreply.github.com>
2024-11-11 21:22:56 +0100

    chore: change positional arguments to named arguments

--- src/error.rs
+++ src/error.rs
@@ -3,27 +3,27 @@ use crate::protocol;
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error(
-        "The channel value {0} is outside the permissible range of {} to {}",
-        protocol::CHANNELS_MIN,
-        protocol::CHANNELS_MAX
+        "The channel value {0} is outside the permissible range of {min} to {max}",
+        min = protocol::CHANNELS_MIN,
+        max = protocol::CHANNELS_MAX
     )]
     ChannelOutOfRange(u8),
     #[error(
-        "The degree celsius value {0} is outside the permissible range of {} to {}",
-        protocol::DEGREE_CELSIUS_MIN,
-        protocol::DEGREE_CELSIUS_MAX
+        "The degree celsius value {0} is outside the permissible range of {min} to {max}",
+        min = protocol::DEGREE_CELSIUS_MIN,
+        max = protocol::DEGREE_CELSIUS_MAX
     )]
     DegreeCelsiusOutOfRange(f32),
     #[error(
-        "The duration value {0} is outside the permissible range of {} to {}",
-        protocol::DURATION_MIN,
-        protocol::DURATION_MAX
+        "The duration value {0} is outside the permissible range of {min} to {max}",
+        min = protocol::DURATION_MIN,
+        max = protocol::DURATION_MAX
     )]
     DurationOutOfRange(u8),
     #[error(
-        "The address value {0} is outside the permissible range of {} to {}",
-        protocol::ADDRESS_MIN,
-        protocol::ADDRESS_MAX
+        "The address value {0} is outside the permissible range of {min} to {max}",
+        min = protocol::ADDRESS_MIN,
+        max = protocol::ADDRESS_MAX
     )]
     AddressOutOfRange(u8),
 }
